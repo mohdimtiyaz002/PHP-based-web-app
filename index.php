8a5fa50701da8df2d4f119bf7afc0343d372d16e
mohdimtiyaz002 <76655817+mohdimtiyaz002@users.noreply.github.com>
2023-08-08 21:17:46 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Web App</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to My PHP Web App</h1>
+    </header>
+    <main>
+        <p>This is a simple PHP web application.</p>
+        <a href="login.php">Login</a> | <a href="register.php">Register</a>
+    </main>
+    <footer>
+        <p>&copy; 2023 My PHP Web App</p>
+    </footer>
+</body>
+</html>
